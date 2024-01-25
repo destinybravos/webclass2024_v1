9bc63aec7253402208348ad85b3a64b287042633
destinybravos <destinybravos@gmail.com>
2024-01-25 15:34:34 +0100

    Added tailwindcss using node js

---
+++ class_01_tailwind_projects/project2/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./pages/**/*.{html,js,php,css}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#004250',
+      }
+    },
+  },
+  plugins: [],
+}
+
