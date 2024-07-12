1e5b30da8bdaab1fda9a7726d951f7dd970be04f
destinybravos <destinybravos@gmail.com>
2024-07-12 17:56:58 +0100

    Did the fetch and Insert

--- backend/testapp/db/fetch.php
+++ backend/testapp/db/fetch.php
@@ -1,15 +1,32 @@
 <?php
     header('Content-Type: application/json');
     include_once 'connect.php';
+    $response = array();
 
+    // $fetchQuery = $conn->query("SELECT id, firstname, lastname, email as email_address FROM users");
     $fetchQuery = $conn->query("SELECT * FROM users");
       
+    $users = array();
     
     if ($fetchQuery->num_rows > 0) {
-        echo json_encode($fetchQuery->fetch_assoc());
+
+        
+        while ($user = $fetchQuery->fetch_assoc()) {
+            array_push($users, $user);
+        }
+
+        $response['success'] = true;
+        $response['users'] = $users;
+        $response['message'] = 'Users records fetched successfully!';
+
     } else {
-        echo "No User Record Found";
+        $response['success'] = false;
+        $response['users'] = [];
+        $response['message'] = 'No user record found!';
     }
+
+
+    echo json_encode($response);
 
 
 
--- backend/testapp/db/insert.php
+++ backend/testapp/db/insert.php
@@ -1,25 +1,49 @@
 <?php
-
+    header('Content-Type: application/json');
     include_once 'connect.php';
-
 
 
     // $insertQuery = $conn->query("INSERT INTO users (firstname, lastname, phone, email)
     //                         VALUES ('Jude', 'Kngsley', '08023456789', 'judekings@gmail.com')");
 
-    $firstname = 'Jude';
-    $lastname = 'Kngsley';
-    $phone = '08023456789';
-    $email = 'judekings@gmail.com';
+    try {
 
-    $insertQuery = $conn->query("INSERT INTO users (firstname, lastname, phone, email)
-                            VALUES ('$firstname', '$lastname', '$phone', '$email')");
+        if ((!isset($_POST['firstname']) || $_POST['firstname'] == '') || (!isset($_POST['phone']) || $_POST['phone'] == '') || (!isset($_POST['email']) || $_POST['email'] == '')) {
+            throw new Exception("Invalid or Missing parameters. Firstname, Lastname, Phone and Email are required", 422);
+        }
+
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $phone = $_POST['phone'];
+        $email = $_POST['email'];
+        $gender = isset($_POST['gender']) ? $_POST['gender'] : null;
+        $age = isset($_POST['age']) ? $_POST['age'] : null;
+        $bio = isset($_POST['bio']) ? $_POST['bio'] : null;
+
+        
+        $insertQuery = $conn->query("INSERT INTO users (firstname, lastname, phone, email, gender, age, bio)
+                            VALUES ('$firstname', '$lastname', '$phone', '$email', '$gender', '$age', '$bio')");
       
       
-    if ($insertQuery) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . "<br>" . $conn->error;
+        if ($insertQuery) {
+            header('HTTP/1 200 Ok');
+            echo json_encode([
+                'success' => true,
+                'message' => 'record added successfully'
+            ]);
+        } else {
+            header('HTTP/1 500 Server Error');
+            echo json_encode([
+                'success' => false,
+                'message' => "Error: " . $conn->error
+            ]);
+        }
+    } catch (\Throwable $th) {
+        header('HTTP/1 ' . $th->getCode());
+        echo json_encode([
+            'success' => false,
+            'message' => $th->getMessage()
+        ]);
     }
 
 
